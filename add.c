0b4e56f3040ca20150c35a599bd084960568c6de
1815125628 <hyh1815125628@qq.com>
2024-10-25 10:53:45 +0800

    Add files via upload

--- add.c
+++ add.c
@@ -1,2 +1,11 @@
 //#define _CRT_SECURE_NO_WARNINGS
 //int a = 100;
+//static int a = 100;
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//static int Add(int x, int y)
+//{
+//	return x + y;
+//}
